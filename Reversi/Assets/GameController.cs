fe947d37f59a095a770a440b5300d8c3f18f38cc
Andrew Bowler <bowlerandrewj@gmail.com>
2016-04-09 20:41:45 -0500

    Pressing Play starts the game, closes main menu

--- Reversi/Assets/GameController.cs
+++ Reversi/Assets/GameController.cs
@@ -1,9 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 
 public class GameController : MonoBehaviour {
     public Board board;
     public Piece[] pieces;
+    public GameObject mainMenu;
+    public EventSystem eventSystem;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        GameObject currentSelection = eventSystem.currentSelectedGameObject;
+        if (currentSelection == GameObject.Find("Play Button"))
+        {
+            mainMenu.SetActive(false);
+        }
+    }
 
     void DeterminePlacementCoordinates()
     {
