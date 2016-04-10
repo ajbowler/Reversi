198bcfb8c00db741296221d4773abda363807678
Andrew Bowler <bowlerandrewj@gmail.com>
2016-04-09 20:55:25 -0500

    Difficulty and color, main menu is all done

--- Reversi/Assets/GameController.cs
+++ Reversi/Assets/GameController.cs
@@ -1,12 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
     public Board board;
     public Piece[] pieces;
     public GameObject mainMenu;
+    public Dropdown colorSelector;
+    public Dropdown difficultySelector;
     public EventSystem eventSystem;
+
+    private int difficulty;
+    private string playerColor;
 
     void Start()
     {
@@ -19,6 +25,10 @@ public class GameController : MonoBehaviour {
         if (currentSelection == GameObject.Find("Play Button"))
         {
             mainMenu.SetActive(false);
+            difficulty = difficultySelector.value + 1;
+            if (colorSelector.value == 0) playerColor = "black";
+            else playerColor = "white";
+            Debug.Log(playerColor);
         }
     }
 
