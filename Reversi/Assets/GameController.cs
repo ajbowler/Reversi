aa9261dc1d149c3c3a33e0f9a7211e8eef7cc8cd
Andrew Bowler <bowlerandrewj@gmail.com>
2016-04-09 20:06:36 -0500

    Add Square prefab to display available moves

--- Reversi/Assets/GameController.cs
+++ Reversi/Assets/GameController.cs
@@ -3,4 +3,10 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
     public Board board;
+    public Piece[] pieces;
+
+    void DeterminePlacementCoordinates()
+    {
+
+    }
 }
