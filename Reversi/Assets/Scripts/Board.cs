0147ce887d973739dcdf1bd13eefb4624804a987
Andrew Bowler <bowlerandrewj@gmail.com>
2016-04-12 10:22:44 -0500

    Piece flipping works, store them in a dictionary

--- Reversi/Assets/Scripts/Board.cs
+++ Reversi/Assets/Scripts/Board.cs
@@ -7,13 +7,13 @@ public class Board : MonoBehaviour {
     public Square squarePrefab;
     public Piece piecePrefab;
     public List<Square> squares;
-    public List<Piece> pieces;
+    public Dictionary<int, Piece> pieces;
     public List<int> legalMoves;
 
     void Start()
     {
         squares = new List<Square>();
-        pieces = new List<Piece>();
+        pieces = new Dictionary<int, Piece>();
         legalMoves = new List<int>();
     }
 
@@ -32,7 +32,12 @@ public class Board : MonoBehaviour {
     {
         squares[position].isLegalMove = false;
         squares[position].player = player;
-        PlacePiece(squares[position]);
+        Piece existingPiece;
+        if (pieces.TryGetValue(position, out existingPiece))
+            pieces[position].gameObject.transform.Rotate(180f, 0f, 0f);
+        else
+            PlacePiece(squares[position]);
+
     }
 
     public void FlipPiece(Player player, int position)
@@ -53,13 +58,14 @@ public class Board : MonoBehaviour {
 
     void PlacePiece(Square s)
     {
-        Vector3 position = s.transform.position;
-        position.y = 1f;
+        Vector3 piecePosition = s.transform.position;
+        piecePosition.y = 1f;
+        Piece newPiece;
         if (s.player == Player.White)
-            Instantiate(piecePrefab, s.transform.position, Quaternion.identity);
+            newPiece = Instantiate(piecePrefab, piecePosition, Quaternion.identity) as Piece;
         else
-            Instantiate(piecePrefab, s.transform.position, Quaternion.AngleAxis(180f, Vector3.right));
-        pieces.Add(piecePrefab);
+            newPiece = Instantiate(piecePrefab, piecePosition, Quaternion.AngleAxis(180f, Vector3.right)) as Piece;
+        pieces.Add(s.position, newPiece);
     }
 
     void InitializeTile(int position, Player player)
