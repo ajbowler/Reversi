38d0ea1b77cbc8911eee20bf5e7b32d21eb6d240
Andrew Bowler <bowlerandrewj@gmail.com>
2016-04-23 12:40:26 -0500

    Remove all logic from the Board, this removes need for instantiating in game tree

--- Reversi/Assets/Scripts/GameController.cs
+++ Reversi/Assets/Scripts/GameController.cs
@@ -3,6 +3,13 @@ using System.Collections;
 using UnityEngine.UI;
 using System.Collections.Generic;
 
+public enum Player
+{
+    Black,
+    White,
+    Nobody
+};
+
 public class MinimaxPair<First, Second>
 {
     public First bestMove { get; set; }
@@ -23,6 +30,10 @@ public class MinimaxPair<First, Second>
 public class GameController : MonoBehaviour
 {
     public Board board;
+    public Square[] squares;
+    public Dictionary<int, Piece> pieces;
+    public Piece piecePrefab;
+    public Square squarePrefab;
     public GameObject mainMenu;
     public GameObject gameOverMenu;
     public Text blackScoreText;
@@ -32,6 +43,7 @@ public class GameController : MonoBehaviour
     public Dropdown difficultySelector;
     public Player human;
     public Player ai;
+    public Player[] playerMap;
 
     private int difficulty;
     private int blackScore;
@@ -41,6 +53,9 @@ public class GameController : MonoBehaviour
     void Start()
     {
         ply = Player.Black;
+        squares = new Square[64];
+        playerMap = new Player[64];
+        pieces = new Dictionary<int, Piece>();
     }
 
     void Update()
@@ -51,11 +66,11 @@ public class GameController : MonoBehaviour
 
     IEnumerator PlayGame()
     {
-        if (!board.HasLegalMoves())
+        if (!HasLegalMoves())
             GameOver();
         else
         {
-            board.UpdatePieces();
+            UpdatePieces();
             UpdateSquareDisplays();
             UpdateScore();
             if (ply == human) yield return StartCoroutine(HumanTurn());
@@ -68,9 +83,9 @@ public class GameController : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             int clickedSquare = GetClickedSquare();
-            if (clickedSquare > -1 && board.squares[clickedSquare].isLegalMove)
+            if (clickedSquare > -1 && squares[clickedSquare].isLegalMove)
             {
-                MakeMove(board, ply, clickedSquare);
+                MakeMove(playerMap, ply, clickedSquare);
                 SetPly(ai);
             }
         }
@@ -92,9 +107,9 @@ public class GameController : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             int clickedSquare = GetClickedSquare();
-            if (clickedSquare > -1 && board.squares[clickedSquare].isLegalMove)
+            if (clickedSquare > -1 && squares[clickedSquare].isLegalMove)
             {
-                MakeMove(board, ply, clickedSquare);
+                MakeMove(playerMap, ply, clickedSquare);
                 SetPly(human);
             }
         }
@@ -118,18 +133,29 @@ public class GameController : MonoBehaviour
                 human = Player.White;
                 ai = Player.Black;
             }
-            board.SetInitialBoard();
+            SetInitialBoard();
             blackScoreText.gameObject.SetActive(true);
             whiteScoreText.gameObject.SetActive(true);
             SetPly(Player.Black);
         }
     }
 
-    void GameOver()
+    public void SetInitialBoard()
     {
-        board.gameObject.SetActive(false);
-        board.Reset();
+        board.gameObject.SetActive(true);
+        for (int i = 0; i< 64; i++)
+        {
+            if (i == 27 || i == 36) InitializeTile(i, Player.Black);
+            else if (i == 28 || i == 35) InitializeTile(i, Player.White);
+            else InitializeTile(i, Player.Nobody);
+        }
+    }
 
+
+    public void GameOver()
+    {
+        Reset();
+        board.gameObject.SetActive(false);
         if (blackScore > whiteScore)
             winnerText.text = "Black Wins!";
         else if (whiteScore > blackScore)
@@ -145,26 +171,26 @@ public class GameController : MonoBehaviour
         }
     }
 
-    void MakeMove(Board board, Player ply, int position)
+    public void MakeMove(Player[] playerMap, Player ply, int position)
     {
-        if (board.squares[position].isLegalMove)
+        if (squares[position].isLegalMove)
         {
-            board.CaptureTile(board.playerMap, ply, position);
-            board.FlankPieces(board.playerMap, ply, position);
+            CaptureTile(playerMap, ply, position);
+            FlankPieces(playerMap, ply, position);
         }
     }
 
-    void SetPly(Player player)
+    public void SetPly(Player player)
     {
         ply = player;
-        board.UpdateLegalMoves(ply);
+        UpdateLegalMoves(playerMap, ply);
     }
 
-    void UpdateScore()
+    public void UpdateScore()
     {
         blackScore = 0;
         whiteScore = 0;
-        foreach (Player player in board.playerMap)
+        foreach (Player player in playerMap)
         {
             if (player == Player.Black)
                 blackScore++;
@@ -174,6 +200,24 @@ public class GameController : MonoBehaviour
 
         blackScoreText.text = "Black: " + blackScore;
         whiteScoreText.text = "White: " + whiteScore;
+    }
+
+    public void UpdatePieces()
+    {
+        for (int i = 0; i < 64; i++)
+        {
+            if (playerMap[i] != Player.Nobody)
+            {
+                Piece existingPiece;
+                if (!pieces.TryGetValue(i, out existingPiece))
+                    PlacePiece(i);
+                else
+                {
+                    if (pieces[i].player != playerMap[i])
+                        FlipPiece(playerMap[i], pieces[i]);
+                }
+            }
+        }
     }
 
     //MinimaxPair<Player[], double> Minimax(Player[] boardMap, int depth, bool maximizingPlayer)
@@ -222,13 +266,177 @@ public class GameController : MonoBehaviour
 
     public void UpdateSquareDisplays()
     {
-        foreach (Square s in board.squares)
+        foreach (Square s in squares)
         {
             if (s.isLegalMove)
                 s.gameObject.GetComponent<MeshRenderer>().enabled = true;
             else
                 s.gameObject.GetComponent<MeshRenderer>().enabled = false;
         }
+    }
+
+    public void CaptureTile(Player[] map, Player player, int position)
+    {
+        squares[position].isLegalMove = false;
+        map[position] = player;
+    }
+
+    public void FlankPieces(Player[] map, Player currentPlayer, int position)
+    {
+        List<int> flankedPieces = new List<int>();
+        flankedPieces.AddRange(AddFlankedPieces(map, currentPlayer, position, -1));
+        flankedPieces.AddRange(AddFlankedPieces(map, currentPlayer, position, 1));
+        flankedPieces.AddRange(AddFlankedPieces(map, currentPlayer, position, -8));
+        flankedPieces.AddRange(AddFlankedPieces(map, currentPlayer, position, 8));
+        flankedPieces.AddRange(AddFlankedPieces(map, currentPlayer, position, -9));
+        flankedPieces.AddRange(AddFlankedPieces(map, currentPlayer, position, 9));
+        flankedPieces.AddRange(AddFlankedPieces(map, currentPlayer, position, -7));
+        flankedPieces.AddRange(AddFlankedPieces(map, currentPlayer, position, 7));
+
+        for (int i = 0; i < flankedPieces.Count; i++)
+            CaptureTile(map, currentPlayer, flankedPieces[i]);
+    }
+
+    public void Reset()
+    {
+        squares = new Square[64];
+        playerMap = new Player[64];
+        pieces.Clear();
+        GameObject[] pieceObjects = GameObject.FindGameObjectsWithTag("Piece");
+        GameObject[] squareObjects = GameObject.FindGameObjectsWithTag("Square");
+        for (int i = 0; i < pieceObjects.Length; i++)
+            Destroy(pieceObjects[i]);
+        for (int i = 0; i < squareObjects.Length; i++)
+            Destroy(squareObjects[i]);
+    }
+
+    public void FlipPiece(Player player, Piece piece)
+    {
+        piece.player = player;
+        piece.gameObject.transform.Rotate(180f, 0f, 0f);
+    }
+
+    public void PlacePiece(int position)
+    {
+        Vector3 piecePosition = squares[position].transform.position;
+        piecePosition.y = 1f;
+        Piece newPiece;
+        if (playerMap[squares[position].position] == Player.White)
+            newPiece = Instantiate(piecePrefab, piecePosition, Quaternion.identity) as Piece;
+        else
+            newPiece = Instantiate(piecePrefab, piecePosition, Quaternion.AngleAxis(180f, Vector3.right)) as Piece;
+        newPiece.player = playerMap[squares[position].position];
+        pieces.Add(squares[position].position, newPiece);
+    }
+
+    public void InitializeTile(int position, Player player)
+    {
+        playerMap[position] = player;
+
+        Vector3 squarePos = DeterminePlacementCoordinates(position);
+        squarePos.y += .2f;
+        Square s = Instantiate(squarePrefab, squarePos, Quaternion.identity) as Square;
+        s.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        s.position = position;
+        s.isLegalMove = false;
+        squares[position] = s;
+        if (player != Player.Nobody)
+            PlacePiece(position);
+    }
+
+    public void UpdateLegalMoves(Player[] map, Player currentPlayer)
+    {
+        ResetLegalMoves();
+        for (int i = 0; i < 64; i++)
+        {
+            if (map[i] == currentPlayer)
+            {
+                AddLegalMove(map, currentPlayer, i, -1);
+                AddLegalMove(map, currentPlayer, i, 1);
+                AddLegalMove(map, currentPlayer, i, -8);
+                AddLegalMove(map, currentPlayer, i, 8);
+                AddLegalMove(map, currentPlayer, i, -9);
+                AddLegalMove(map, currentPlayer, i, 9);
+                AddLegalMove(map, currentPlayer, i, -7);
+                AddLegalMove(map, currentPlayer, i, 7);
+            }
+        }
+    }
+
+    private void AddLegalMove(Player[] map, Player player, int position, int direction)
+    {
+        bool flankablesExist = false;
+
+        for (int i = position + direction; i >= 0 && i <= 63; i += direction)
+        {
+            if (IsPastBoardEdge(position, position, direction))
+                return; // we can't go this way
+            if (map[i] != player && map[i] != Player.Nobody)
+                flankablesExist = true;
+            else if (map[i] == Player.Nobody && flankablesExist)
+            {
+                squares[i].isLegalMove = true;
+                return;
+            }
+            else return; // we can't go this way
+        }
+    }
+
+    public void ResetLegalMoves()
+    {
+        foreach (Square s in squares)
+            if (s.isLegalMove)
+                s.isLegalMove = false;
+    }
+
+    public bool HasLegalMoves()
+    {
+        for (int i = 0; i < 64; i++)
+        {
+            if (squares[i] != null)
+            {
+                if (squares[i].isLegalMove)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private Vector3 DeterminePlacementCoordinates(int position)
+    {
+        int row = position / 8;
+        int col = position % 8;
+
+        float x = row - 3.5f;
+        float z = col - 3.5f;
+
+        return new Vector3(x, 1f, z);
+    }
+
+    private List<int> AddFlankedPieces(Player[] playerMap, Player currentPlayer, int position, int direction)
+    {
+        List<int> flankedPieces = new List<int>();
+
+        for (int i = position + direction; i >= 0 && i <= 63; i += direction)
+        {
+            if (playerMap[i] != currentPlayer && playerMap[i] != Player.Nobody)
+                flankedPieces.Add(i);
+            else if (playerMap[i] == currentPlayer)
+                break; // we have flanked everything we can in this direction
+            else if (playerMap[i] == Player.Nobody || IsPastBoardEdge(position, i, direction))
+            {
+                flankedPieces.Clear();
+                break; // nothing here can be flanked
+            }
+        }
+
+        return flankedPieces;
+    }
+
+    private bool IsPastBoardEdge(int start, int end, int direction)
+    {
+        if (direction > 0) return ((start % 8) - (end % 8)) > 0;
+        else return ((start % 8) - (end % 8)) < 0;
     }
 
     private int GetClickedSquare()
